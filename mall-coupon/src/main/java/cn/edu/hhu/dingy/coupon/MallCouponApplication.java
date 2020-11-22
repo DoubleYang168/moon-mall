51e2af5a0de2e2bafd8e2e27522ac2096d0c5f9d
DoubleYang168 <1248151977@qq.com>
2020-11-22 20:04:00 +0800

    项目初始结构创建

---
+++ mall-coupon/src/main/java/cn/edu/hhu/dingy/coupon/MallCouponApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hhu.dingy.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponApplication.class, args);
+    }
+
+}
