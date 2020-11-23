9863ebe1ef1237038219cc82e7f63989ab62ce6d
DoubleYang168 <1248151977@qq.com>
2020-11-23 11:08:29 +0800

    项目初始结构创建

--- mall-product/src/main/java/cn/edu/hhu/dingy/product/MallProductApplication.java
+++ mall-product/src/main/java/cn/edu/hhu/dingy/product/MallProductApplication.java
@@ -1,8 +1,11 @@
 package cn.edu.hhu.dingy.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@MapperScan("cn.edu.hhu.dingy.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
--- mall-product/src/test/java/cn/edu/hhu/dingy/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/cn/edu/hhu/dingy/product/MallProductApplicationTests.java
@@ -1,13 +1,22 @@
 package cn.edu.hhu.dingy.product;
 
+import cn.edu.hhu.dingy.product.entity.BrandEntity;
+import cn.edu.hhu.dingy.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("华为");
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
